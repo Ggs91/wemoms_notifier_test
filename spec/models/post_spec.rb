a528d3c04cd057ed7836d4e7b9396fc369c6a275
Ggs91 <georges_atalla@hotmail.fr>
2021-09-12 23:04:33 +0200

    add validation to description attribute

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { should belong_to(:user) }
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:description).with_message('Description must be present') }
+  end
 end
