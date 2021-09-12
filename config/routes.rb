fc648a04947648ce9831f45e9f9a93bccd670a15
Ggs91 <georges_atalla@hotmail.fr>
2021-09-12 23:04:06 +0200

    add create action to posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :users, only: [:index]
-      resources :posts, only: [:index]
+      resources :posts, only: [:index, :create]
     end
   end
 end
