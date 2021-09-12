206982b4a27de94fe3a177e7fc4a7889a03fdd18
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 00:04:16 +0200

    includes user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostsController < Api::V1::BaseController
   def index
-    posts = Post.all.page(page).per(per_page)
+    posts = Post.includes(:user).page(page).per(per_page)
 
     render json: posts
   end
