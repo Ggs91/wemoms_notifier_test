54d2e3220c55f7dd05b8bae5008173f8e6832d5c
Ggs91 <georges_atalla@hotmail.fr>
2021-09-12 23:02:57 +0200

    dry up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def index
-    users = User.all
-    render json: users, each_serializer: Api::UserSerializer
+    users = User.all.page(page).per(per_page)
+
+    render json: users
   end
 end
