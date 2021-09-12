408649a5888cf0ac482d98d7d7be9703be25fcd6
Ggs91 <georges_atalla@hotmail.fr>
2021-09-12 23:03:12 +0200

    dry up

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,23 @@
 class Api::V1::PostsController < Api::V1::BaseController
   def index
-    p "ù"*60
-    p current_user
-    p "ù"*60
-    posts = Post.all
-    render json: posts, each_serializer: Api::PostSerializer
+    posts = Post.all.page(page).per(per_page)
+
+    render json: posts
+  end
+
+  def create
+    post = Post.create!(post_params.merge(user: current_user))
+
+    render json: post, links: link(post), serializer: Api::V1::StatusCreatedSerializer, status: :created
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+
+  def link(post)
+    {self: api_v1_posts_url(post)}
   end
 end
