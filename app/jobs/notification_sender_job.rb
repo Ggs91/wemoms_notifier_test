af4e259530aa99589b57ba421bbe76b79bd78e2f
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 16:42:20 +0200

    move logic to Notifier module

--- app/jobs/notification_sender_job.rb
+++ app/jobs/notification_sender_job.rb
@@ -1,12 +1,9 @@
 class NotificationSenderJob < ApplicationJob
+  include Notifier
+
   queue_as :default
 
   def perform(resource_id, resource_type)
-    resource_class = resource_type.constantize
-    resource = resource_class.find(resource_id)
-
-    resource.recipients_ids.each do |recipient_id|
-      Notification.create(resource: resource, user_id: recipient_id)
-    end
+    notify(resource_id, resource_type)
   end
 end
