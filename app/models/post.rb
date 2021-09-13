fd007e111b4c386048d572b2a2466e1f17109145
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 16:42:37 +0200

    remove useless private keyword

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@ class Post < ApplicationRecord
   belongs_to :user
   validates_presence_of :description, message: "Description must be present"
 
-  private
-
   def recipients_ids
     User.pluck(:id)
   end
