ce4dbb0babc2704744680b1577e90e45120111be
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 12:38:41 +0200

    implement #recipients_ids

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   include Notifiable
   belongs_to :user
   validates_presence_of :description, message: "Description must be present"
+
+  private
+
+  def recipients_ids
+    User.pluck(:id)
+  end
 end
