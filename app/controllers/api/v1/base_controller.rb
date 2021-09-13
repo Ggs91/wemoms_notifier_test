2aeb35ddc96641c685073ea49506187484b1ccec
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 16:40:53 +0200

    implement current_user

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,7 +2,16 @@ class Api::V1::BaseController < ApplicationController
   before_action :set_default_format
   before_action :set_serializer_namespace
 
+  # To simplify the test, I'm using the first user in database
+  def current_user
+    find_or_create_user
+  end
+
   private
+
+  def find_or_create_user
+    User.first || User.create
+  end
 
   def set_default_format
     request.format = :json
