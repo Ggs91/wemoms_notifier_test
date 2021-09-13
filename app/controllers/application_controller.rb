d09bf1dcb178de2efd9797cb476069c8eebf760a
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 16:41:58 +0200

    remove current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,6 @@ class ApplicationController < ActionController::API
   before_action :require_current_user
   serialization_scope :current_user
 
-  # To simplify the test, I'm using the first user in database
-  def current_user
-    User.first
-  end
-
-  private
-
   def require_current_user
     return if current_user
     render json: { error: 'you must be logged in' }, status: :unauthorized
