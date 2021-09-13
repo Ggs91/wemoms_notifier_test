a368b12f78b95e6b60514b6fa1799d562ae5fdc6
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 16:43:23 +0200

    implement notification job logic

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,6 +1,11 @@
 module Notifier
-  def notify(post)
-    puts "Send notification"
+  def notify(resource_id, resource_type)
+    resource = find_resource(resource_id, resource_type)
+
+    resource.recipients_ids.each do |recipient_id|
+      next if notification_already_sent?(resource, recipient_id)
+      Notification.create(resource: resource, user_id: recipient_id)
+    end
   end
 
   def notifications_sent_count(post)
@@ -9,5 +14,16 @@ module Notifier
 
   def notifications_opened_count(post)
     return 10
+  end
+
+  private
+
+  def notification_already_sent?(resource, recipient_id)
+    Notification.exists?(resource: resource, user_id: recipient_id)
+  end
+
+  def find_resource(resource_id, resource_type)
+    resource_class = resource_type.constantize
+    resource_class.find(resource_id)
   end
 end
