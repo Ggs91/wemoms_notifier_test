5ee7a549e8c6dd14c49e125f63231a80a18b7611
Ggs91 <georges_atalla@hotmail.fr>
2021-09-13 17:54:31 +0200

    add read & unread scopes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :resource, polymorphic: true
+
+  scope :unread, -> { where(read: nil) }
+  scope :read, -> { where.not(read: nil) }
 end
