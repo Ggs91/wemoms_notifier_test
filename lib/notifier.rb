11a5bf4e5779835638a199a0b89852127142ca49
Ggs91 <georges_atalla@hotmail.fr>
2021-09-14 15:58:29 +0200

    separate logic into dedicated modules

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,25 +1,7 @@
 module Notifier
+  extend ActiveSupport::Autoload
 
-  extend self
-
-  def notify(resource)
-    resource.recipients_ids.each do |recipient_id|
-      next if notification_already_sent?(resource, recipient_id)
-      Notification.create(resource: resource, user_id: recipient_id)
-    end
-  end
-
-  def notifications_sent_count(resource)
-    resource.notifications.count
-  end
-
-  def notifications_opened_count(resource)
-    resource.notifications.opened.count
-  end
-
-  private
-
-  def notification_already_sent?(resource, recipient_id)
-    Notification.exists?(resource: resource, user_id: recipient_id)
-  end
+  autoload :Notifiable, "notifier/resource"
+  autoload :Notifiable, "notifier/notifiable"
+  autoload :Notifier, "notifier/metrics"
 end
