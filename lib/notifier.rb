bbd40bac8058418b3d671045ca3fe68bf95a2163
Ggs91 <georges_atalla@hotmail.fr>
2021-09-14 11:15:09 +0200

    change instance methode to module methods

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,7 +1,8 @@
 module Notifier
-  def notify(resource_id, resource_type)
-    resource = find_resource(resource_id, resource_type)
 
+  extend self
+
+  def notify(resource)
     resource.recipients_ids.each do |recipient_id|
       next if notification_already_sent?(resource, recipient_id)
       Notification.create(resource: resource, user_id: recipient_id)
@@ -20,10 +21,5 @@ module Notifier
 
   def notification_already_sent?(resource, recipient_id)
     Notification.exists?(resource: resource, user_id: recipient_id)
-  end
-
-  def find_resource(resource_id, resource_type)
-    resource_class = resource_type.constantize
-    resource_class.find(resource_id)
   end
 end
