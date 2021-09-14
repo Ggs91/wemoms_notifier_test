faefb13de3590a6b9a9e2ac6e64da2101d83f2c6
Ggs91 <georges_atalla@hotmail.fr>
2021-09-14 12:54:21 +0200

    implement notification metrics methods

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -9,12 +9,12 @@ module Notifier
     end
   end
 
-  def notifications_sent_count(post)
-    return 100
+  def notifications_sent_count(resource)
+    resource.notifications.count
   end
 
-  def notifications_opened_count(post)
-    return 10
+  def notifications_opened_count(resource)
+    resource.notifications.opened.count
   end
 
   private
