76f8db8996049a84bb518c1d5fe6fcde9b52d52d
Ggs91 <georges_atalla@hotmail.fr>
2021-09-14 11:13:51 +0200

    find resource from its id & type

--- app/jobs/notification_sender_job.rb
+++ app/jobs/notification_sender_job.rb
@@ -1,9 +1,16 @@
 class NotificationSenderJob < ApplicationJob
-  include Notifier
 
   queue_as :default
 
   def perform(resource_id, resource_type)
-    notify(resource_id, resource_type)
+    resource = find_resource(resource_id, resource_type)
+    Notifier.notify(resource)
+  end
+
+  private
+
+  def find_resource(resource_id, resource_type)
+    resource_class = resource_type.constantize
+    resource_class.find(resource_id)
   end
 end
