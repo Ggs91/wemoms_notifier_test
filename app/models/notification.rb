6637fec08af8a5033a3d73b26e92c006dc7d5054
Ggs91 <georges_atalla@hotmail.fr>
2021-09-14 12:42:04 +0200

    change scope name to opened_at

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :resource, polymorphic: true
 
-  scope :not_opened, -> { where(seen_at: nil) }
-  scope :opened, -> { where.not(seen_at: nil) }
+  scope :not_opened, -> { where(opened_at: nil) }
+  scope :opened, -> { where.not(opened_at: nil) }
 end
